8967227c0cf93cc871670bed7c87dc1db0ad6d3b
rubbyhan <49549566+rubbyhan@users.noreply.github.com>
2021-11-21 21:22:29 +0700

    Update score.blade.php

--- resources/views/score.blade.php
+++ resources/views/score.blade.php
@@ -7,7 +7,7 @@
         <h4 class="text-center">{{ $response['RIASEC1'] }}, {{ $response['RIASEC2'] }}, and {{ $response['RIASEC3'] }}</h4>
         <h4 class="text-center">{{ $response['IE'] }}, {{ $response['SN'] }}, {{ $response['TF'] }}, {{ $response['PJ'] }}</h4>
         <h1 class="text-center">Di mohon untuk mengisi kuisioner berikut</h1>
-        <a  class="text-center" href="https://docs.google.com/forms/d/e/1FAIpQLSf_KdF9MFZ7XIejOHHLafUTG3OJbq9ecRif8VvNfsAgfKj_aw/viewform?usp=sf_link">kuisioner</a>.</p>
+        <a  class="text-center",href="https://docs.google.com/forms/d/e/1FAIpQLSf_KdF9MFZ7XIejOHHLafUTG3OJbq9ecRif8VvNfsAgfKj_aw/viewform?usp=sf_link">kuisioner</a>
         <a href="{{ route('index') }}" class="btn btn-success btn-lg btn-block">Restart Test</a>
     </div>
 @endsection
