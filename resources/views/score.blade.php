03f459d14cd9633b44059317b031bc167c0063c3
rubbyhan <49549566+rubbyhan@users.noreply.github.com>
2021-11-21 21:24:26 +0700

    Update score.blade.php

--- resources/views/score.blade.php
+++ resources/views/score.blade.php
@@ -3,9 +3,10 @@
     <div class="container">
         <h1 class="text-center">Rekomendasi pekerjaan anda</h1>
         <h4 class="text-center">{{ $response['RecomJobs1'] }} atau {{ $response['RecomJobs2'] }}</h4>
-        <h1 class="text-center">You are</h1>
-        <h4 class="text-center">{{ $response['RIASEC1'] }}, {{ $response['RIASEC2'] }}, and {{ $response['RIASEC3'] }}</h4>
+        <h1 class="text-center">Hasil Psikologi anda adalah:</h1>
         <h4 class="text-center">{{ $response['IE'] }}, {{ $response['SN'] }}, {{ $response['TF'] }}, {{ $response['PJ'] }}</h4>
+        <h1 class="text-center">Hasil Minat anda adalah:</h1>
+        <h4 class="text-center">{{ $response['RIASEC1'] }}, {{ $response['RIASEC2'] }}, and {{ $response['RIASEC3'] }}</h4>
         <h1 class="text-center">Di mohon untuk mengisi kuisioner berikut</h1>
         <a  class="text-center" href="https://docs.google.com/forms/d/e/1FAIpQLSf_KdF9MFZ7XIejOHHLafUTG3OJbq9ecRif8VvNfsAgfKj_aw/viewform?usp=sf_link">kuisioner</a>
         <a href="{{ route('index') }}" class="btn btn-success btn-lg btn-block">Restart Test</a>
