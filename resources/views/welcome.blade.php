51a08f11cbb32b3074c5b570d3f3d171ec9c12da
rubbyhan <49549566+rubbyhan@users.noreply.github.com>
2021-11-27 16:01:12 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,9 @@
 @extends('app')
 @section('content')
     <div class="container">
+    <h1 class="text-center">Langkah-Langkah mengisi: </h1>
+    <h1 class="text-center">1. Isilah sesuai dengan kepribadian anda/h1>
+    <h1 class="text-center">2. Setelah mengisi dimohon untuk mengisi kuisioner yang ada dihalaman akhir</h1>
         <form action="{{ route('test.start') }}" method="post">
             @csrf
             <button type="submit" class="btn btn-success btn-block">Start Test</button>
