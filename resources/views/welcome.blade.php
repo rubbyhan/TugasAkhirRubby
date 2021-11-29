f695d99ed302ee68d48d4867bb45563bb0eddce8
rubbyhan <49549566+rubbyhan@users.noreply.github.com>
2021-11-29 15:25:59 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,7 @@
     <h1 class="text-center">1. Isilah sesuai dengan kepribadian anda/h1>
     <h1 class="text-center">2. Setelah mengisi dimohon untuk mengisi kuisioner yang ada dihalaman akhir</h1>
         <form action="{{ route('test.start') }}" method="post">
-        <label for="fname">nama:</label><br>
-        <input type="text" id="fname" name="fname" ><br>
-        <label for="email">E-mail</label><br>
-         <input type="text" id="email" name="email"><br><br>
+       
             @csrf
             <button type="submit" class="btn btn-success btn-block">Start Test</button>
         </form>
