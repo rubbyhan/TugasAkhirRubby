bbe8ba8d45bfc5da825d21fde334b6eaeacb637f
rubbyhan <49549566+rubbyhan@users.noreply.github.com>
2021-11-29 16:08:37 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
     <h1 class="text-center">Langkah-Langkah mengisi: </h1>
-    <h1 class="text-center">1. Isilah sesuai dengan kepribadian anda/h1>
+    <h1 class="text-center">1. Isilah sesuai dengan kepribadian anda</h1>
     <h1 class="text-center">2. Setelah mengisi dimohon untuk mengisi kuisioner yang ada dihalaman akhir</h1>
         <form action="{{ route('test.start') }}" method="post">
        
